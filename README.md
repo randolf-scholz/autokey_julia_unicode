b80d1940a2702ee1b699c616dda550cb8596eed2
Randolf Scholz <rscholz@ismll.de>
2021-08-26 15:02:55 +0200

    v0.1

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
-# autokey_julia_unicode
-Add all JuliaLang unicode abbreviations to AutoKey.
+# Autokey Unicode characters
+
+![demo](output.gif)
+
+## Usage
+
+This script adds all the unicode character abbreviations supported by [Julia](https://docs.julialang.org/en/v1/manual/unicode-input/#Unicode-Input) to autokey. However, instead of `[TAB]`, they are triggered on `[SPACE]`. If you do not want perform the replacement, simply hit `[BACKSPACE]` afterwards.
+
+## Installation
+
+1. Install [autokey](https://github.com/autokey/autokey)
+2. Execute `generate_phrases.py` (requires python >3.7)
+3. Restart autokey
---
+++ generate_phrases.py
@@ -0,0 +1,101 @@
+#!/usr/bin/env python3
+# coding: utf-8
+
+# In[1]:
+
+
+import csv
+import json
+from pathlib import Path
+from string import hexdigits, ascii_letters, digits
+
+
+# In[2]:
+
+
+with open("icons.csv", "r") as csvfile:
+    # scraped from https://docs.julialang.org/en/v1/manual/unicode-input/
+    stream = csv.reader(csvfile, delimiter=";")
+    data = [line for line in stream]
+    header = data[0]
+    unicodes, chars, abbrvs, notes = list(zip(*data[1:]))
+
+
+# In[3]:
+
+
+TEMPLATE = {
+    "abbreviation": {
+        "abbreviations": [],
+        "backspace": True,
+        "ignoreCase": False,
+        "immediate": True,
+        "triggerInside": True,
+        "wordChars": "[^\\t]",
+    },
+    "description": "",
+    "filter": {
+        "regex": None,
+        "isRecursive": False,
+    },
+    "hotkey": {
+        "modifiers": [],
+        "hotKey": None,
+    },
+    "modes": [1],
+    "matchCase": False,
+    "omitTrigger": False,
+    "prompt": False,
+    "sendMode": "<shift>+<insert>",
+    "showInTrayMenu": False,
+    "type": "phrase",
+    "usageCount": 0,
+}
+
+
+# In[4]:
+
+
+ABBRV_CHARS = ascii_letters + digits + ":^/_+-=()!"
+UNICODE_PATTERN = ("U", "+") + (hexdigits,)*5
+
+
+def is_unicode(s):
+    return len(s) == 6 and all(c in cs for c, cs in zip(s, UNICODE_PATTERN))
+
+
+def is_abbrv(s):
+    return len(s) > 1 and s[0] == "\\" and all(c in ABBRV_CHARS for c in s[1:])
+
+
+# In[5]:
+
+
+BASE_PATH = Path.home().joinpath(".config/autokey/data/")
+
+if not BASE_PATH.exists():
+    print(F"{BASE_PATH} does not exist. Will generate files locally instead.")
+    BASE_PATH = Path.cwd()
+else:
+    print(F"Found autokey directory at {BASE_PATH}.")
+
+PATH = BASE_PATH.joinpath("julia_unicode")
+PATH.mkdir(exist_ok=True)
+
+for ucode, char, abbrv, note in data[1:]:
+    abbrvs = abbrv.split(", ")  # sometimes multiple abbreviations exist
+    for abb in abbrvs:
+        assert is_abbrv(abb), F"{abb=} is not valid abbreviation."
+
+    name = abbrvs[0][1:].replace("/", "")   # remove '/' for valid filenames
+
+    # add space at the end, otherwise \delta cannot be typed as \del exists
+    abbrvs = [abb+" " for abb in abbrvs]    
+    TEMPLATE['abbreviation']['abbreviations'] = abbrvs
+    TEMPLATE['description'] = note
+
+    with open(F"{PATH}/.{name}.json", "w") as file:
+        json.dump(TEMPLATE, file, indent=True)
+
+    with open(F"{PATH}/{name}.txt", "w") as file:
+        file.write(char)
