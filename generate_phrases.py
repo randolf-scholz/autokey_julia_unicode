b6d9cc9f02814fabdbe584d79175fe865dad100c
Randolf Scholz <randolf.scholz@gmail.com>
2021-10-26 11:56:22 +0200

    added isntall option to pick insert mode

--- generate_phrases.py
+++ generate_phrases.py
@@ -3,26 +3,17 @@
 
 # In[1]:
 
-
 import csv
 import json
+import sys
 from pathlib import Path
-from string import hexdigits, ascii_letters, digits
-
+from string import ascii_letters, digits, hexdigits
+from typing import Optional
 
 # In[2]:
 
-
-with open("icons.csv", "r") as csvfile:
-    # scraped from https://docs.julialang.org/en/v1/manual/unicode-input/
-    stream = csv.reader(csvfile, delimiter=";")
-    data = [line for line in stream]
-    header = data[0]
-    unicodes, chars, abbrvs, notes = list(zip(*data[1:]))
-
-
-# In[3]:
-
+ABBRV_CHARS = ascii_letters + digits + ":^/_+-=()!"
+UNICODE_PATTERN = ("U", "+") + (hexdigits,) * 5
 
 TEMPLATE = {
     "abbreviation": {
@@ -46,59 +37,11 @@ TEMPLATE = {
     "matchCase": False,
     "omitTrigger": False,
     "prompt": False,
-    "sendMode": "<shift>+<insert>",
+    "sendMode": r"<shift>+<insert>",
     "showInTrayMenu": False,
     "type": "phrase",
     "usageCount": 0,
 }
-
-
-# In[4]:
-
-
-ABBRV_CHARS = ascii_letters + digits + ":^/_+-=()!"
-UNICODE_PATTERN = ("U", "+") + (hexdigits,)*5
-
-
-def is_unicode(s):
-    return len(s) == 6 and all(c in cs for c, cs in zip(s, UNICODE_PATTERN))
-
-
-def is_abbrv(s):
-    return len(s) > 1 and s[0] == "\\" and all(c in ABBRV_CHARS for c in s[1:])
-
-
-# In[5]:
-
-
-BASE_PATH = Path.home().joinpath(".config/autokey/data/")
-
-if not BASE_PATH.exists():
-    print(F"{BASE_PATH} does not exist. Will generate files locally instead.")
-    BASE_PATH = Path.cwd()
-else:
-    print(F"Found autokey directory at {BASE_PATH}.")
-
-PATH = BASE_PATH.joinpath("julia_unicode")
-PATH.mkdir(exist_ok=True)
-
-for ucode, char, abbrv, note in data[1:]:
-    abbrvs = abbrv.split(", ")  # sometimes multiple abbreviations exist
-    for abb in abbrvs:
-        assert is_abbrv(abb), F"{abb} is not valid abbreviation."
-
-    name = abbrvs[0][1:].replace("/", "")   # remove '/' for valid filenames
-
-    # add space at the end, otherwise \delta cannot be typed as \del exists
-    abbrvs = [abb+" " for abb in abbrvs]
-    TEMPLATE['abbreviation']['abbreviations'] = abbrvs
-    TEMPLATE['description'] = note
-
-    with open(F"{PATH}/.{name}.json", "w") as file:
-        json.dump(TEMPLATE, file, indent=True)
-
-    with open(F"{PATH}/{name}.txt", "w") as file:
-        file.write(char)
 
 HELPSCRIPT = """
 import webbrowser
@@ -109,9 +52,7 @@ HELPCONFIG = {
     "type": "script",
     "description": "Help for Julia Unicode",
     "store": {},
-    "modes": [
-        1
-    ],
+    "modes": [1],
     "usageCount": 0,
     "prompt": False,
     "omitTrigger": False,
@@ -124,7 +65,7 @@ HELPCONFIG = {
         "ignoreCase": False,
         "immediate": True,
         "triggerInside": True,
-        "wordChars": "[\\w]"
+        "wordChars": "[\\w]",
     },
     "hotkey": {
         "modifiers": [],
@@ -133,14 +74,104 @@ HELPCONFIG = {
     "filter": {
         "regex": None,
         "isRecursive": False,
-    }
+    },
 }
 
-with open(F"{PATH}/.julia_unicode_help.json", "w") as file:
-    json.dump(HELPCONFIG, file, indent=True)
+# In[3]:
 
-with open(F"{PATH}/julia_unicode_help.py", "w") as file:
-    file.write(HELPSCRIPT)
 
-print(r"Finished Installation. Restart AutoKey to enable.")
-print(r"Type '\help'+[SPACE] for list of all abbreviations.")
+def is_unicode(s: str) -> bool:
+    """Check wether string is a unicode-code (like U+0000A)."""
+    return len(s) == 6 and all(c in cs for c, cs in zip(s, UNICODE_PATTERN))
+
+
+def is_abbrv(s: str) -> bool:
+    """Check if string is a valid abbreviation."""
+    return len(s) > 1 and s[0] == "\\" and all(c in ABBRV_CHARS for c in s[1:])
+
+
+def query_choice(choices: list[str], question: Optional[str] = None) -> str:
+    """Ask the user to pick an option."""
+    if question is None:
+        question = "Please pick one of the following:"
+
+    options: dict[int, str] = dict(enumerate(choices))
+
+    while True:
+        try:
+            print(question)
+            for i, option in options.items():
+                print(f"{i:3d}. {option}")
+            user_choice = input("Enter integer: ")
+        except KeyboardInterrupt:
+            print("Operation aborted. Exiting.")
+            sys.exit(0)
+        else:
+            if user_choice.isdigit() and int(user_choice) in options:
+                break
+            print("Did not understand input.")
+    return options[int(user_choice)]
+
+
+# In[4]:
+
+
+def __main__():
+    BASE_PATH = Path.home().joinpath(".config/autokey/data/")
+
+    with open("icons.csv", "r") as csvfile:
+        # scraped from https://docs.julialang.org/en/v1/manual/unicode-input/
+        stream = csv.reader(csvfile, delimiter=";")
+        data = list(stream)
+        header = data[0]
+        unicodes, chars, abbrvs, notes = list(zip(*data[1:]))
+
+    if not BASE_PATH.exists():
+        print(f"{BASE_PATH} does not exist. Will generate files locally instead.")
+        BASE_PATH = Path.cwd()
+    else:
+        print(f"Found autokey directory at {BASE_PATH}.")
+
+    PATH = BASE_PATH.joinpath("julia_unicode")
+    PATH.mkdir(exist_ok=True)
+
+    sendMode = query_choice(
+        question="How should character substitutions be performed?",
+        choices=[r"<ctrl>+v", r"<ctrl>+<shift>+v", r"<shift>+<insert>"],
+    )
+
+    TEMPLATE["sendMode"] = sendMode
+
+    for ucode, char, abbrv, note in data[1:]:
+        abbrvs = abbrv.split(", ")  # sometimes multiple abbreviations exist
+        for abb in abbrvs:
+            assert is_abbrv(abb), f"{abb} is not valid abbreviation."
+
+        name = abbrvs[0][1:].replace("/", "")  # remove '/' for valid filenames
+
+        # add space at the end, otherwise \delta cannot be typed as \del exists
+        abbrvs = [abb + " " for abb in abbrvs]
+        TEMPLATE["abbreviation"]["abbreviations"] = abbrvs
+        TEMPLATE["description"] = note
+
+        with open(f"{PATH}/.{name}.json", "w") as file:
+            json.dump(TEMPLATE, file, indent=True)
+
+        with open(f"{PATH}/{name}.txt", "w") as file:
+            file.write(char)
+
+    with open(f"{PATH}/.julia_unicode_help.json", "w") as file:
+        json.dump(HELPCONFIG, file, indent=True)
+
+    with open(f"{PATH}/julia_unicode_help.py", "w") as file:
+        file.write(HELPSCRIPT)
+
+    print(r"Finished Installation. Restart AutoKey to enable.")
+    print(r"Type '\help'+[SPACE] for list of all abbreviations.")
+
+
+# In[5]:
+
+
+if __name__ == "__main__":
+    __main__()
